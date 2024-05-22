10631f3e1d516a74683b935111b5ea2885036ffd
Kevin Nguyen <8killernguyen8@gmail.com>
2024-05-22 18:44:59 -0500

    Second part of tutorial
    Progress save

---
+++ Website/my-react-app/src/Card.jsx
@@ -0,0 +1,14 @@
+import profilePic from './assets/KevNFT.png'
+
+function Card()
+{
+    return(
+        <div className="Card">
+            <img className="card-image" src={profilePic} width="150" height="150" alt="Profile Picture"></img>
+            <h2 className="card-title">Kevin Nguyen</h2>
+            <p className="card-text"> I am a programmer and want to make video games</p>
+        </div>
+    );
+}
+
+export default Card
