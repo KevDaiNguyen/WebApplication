ca53ad6d6d2e62cb91cf567400f9193be5291997
Kevin Nguyen <8killernguyen8@gmail.com>
2024-05-23 18:47:21 -0500

    Third and Fourth part of tutorial
    I will try to set the varying types of styles to see which one I prefer.

--- Website/my-react-app/src/App.jsx
+++ Website/my-react-app/src/App.jsx
@@ -2,13 +2,18 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   return(
     <>
       <Header/>
       <Card/>
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student/>
       <Food/>
+      <Button/>
       <Footer/>
     </>
   );
---
+++ Website/my-react-app/src/Student.jsx
@@ -0,0 +1,26 @@
+import Proptype from 'prop-types'
+
+function Student(props)
+{
+    return(
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+        </div>
+    );
+}
+
+Student.prototype = {
+    name: Proptype.string,
+    age: Proptype.number,
+    isStudent: Proptype.bool,
+}
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
+}
+
+export default Student
