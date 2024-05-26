4f20ef3bfe7c0aacce8617839febabc0082eb088
Kevin Nguyen <8killernguyen8@gmail.com>
2024-05-25 19:49:11 -0500

    Tutorial 6 completed
    Very interesting stuff

--- Website/my-react-app/src/App.jsx
+++ Website/my-react-app/src/App.jsx
@@ -5,12 +5,32 @@ import Card from './Card.jsx'
 import Button from './Button.jsx'
 import Student from './Student.jsx'
 import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
 
 function App() {
+  
+  const fruits = [{id: 1, name: "apple", calories: 95}, 
+                    {id: 2, name: "orange", calories: 45}, 
+                    {id: 3, name: "banana", calories: 105}, 
+                    {id: 4, name: "coconut", calories: 159}, 
+                    {id: 5, name: "pineapple", calories: 37}]
+
+  /*
+  const vegetables = [{id: 6, name: "potatoes", calories: 110}, 
+                      {id: 7, name: "celery", calories: 15}, 
+                      {id: 8, name: "carrots", calories: 25}, 
+                      {id: 9, name: "corn", calories: 63}, 
+                      {id: 10, name: "broccoli", calories: 50}]
+  */
+
+  const vegetables = [];
+
   return(
     <>
       <Header/>
       <Card/>
+      {fruits.length > 0 && <List items={fruits} category = "Fruits"/>}
+      {vegetables.length > 0 ? <List items={vegetables} category = "Fruits"></List>: null}
       <Student name="Spongebob" age={30} isStudent={true}/>
       <UserGreeting isLoggedIn={true} username="Random"/>
       <Food/>
