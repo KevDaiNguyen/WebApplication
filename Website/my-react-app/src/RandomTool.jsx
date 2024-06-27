f325396570ecac268405f76797dc2acf61f7a398
Kevin Nguyen <8killernguyen8@gmail.com>
2024-06-27 05:45:16 -0500

    Luck logic is somewhat working
    resetting the odds can cause the value to go into infinity. Trying to figure that out later.

--- Website/my-react-app/src/RandomTool.jsx
+++ Website/my-react-app/src/RandomTool.jsx
@@ -1,39 +1,60 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 
 function RandomTool()
 {
     const [totalChance, setTotalChance] = useState(1);
     const [coinChance, setCoinChance] = useState(1);
     const [dieChance, setDieChance] = useState(1);
+    const [coinFail, setCoinFail] = useState(false);
+    const [dieFail, setDieFail] = useState(false);
+
+    useEffect(() => {
+        formatChance();
+        //console.log(totalChance);
+    })
 
     function increaseOdds(newOdds)
     {
-        setTotalChance(totalChance => totalChance * newOdds);
+        setTotalChance(total => total * newOdds);
     }
     
     function decreaseOdds(oldOdds)
     {
-        setTotalChance(totalChance => totalChance / oldOdds);
+        setTotalChance(total => total / oldOdds);
     }
 
     function increaseCoin()
     {
-        setCoinChance(coinChance => coinChance * (1/2));
+        setCoinChance(coin => coin * (1/2));
     }
 
     function resetCoin()
     {
-        setCoinChance(coinChance => 0);
+        decreaseOdds(coinChance);
+        setCoinChance(0);
     }
 
     function increaseDie()
     {
-        setDieChance(dieChance => dieChance * (1/6));
+        setDieChance(die => die * (1/6));
     }
 
     function resetDie()
     {
-        setDieChance(dieChance => 0);
+        decreaseOdds(dieChance);
+        setDieChance(0);
+    }
+
+    function toggleCoinFail()
+    {
+        setCoinFail(!coinFail);
+        resetCoin();
+    }
+
+    function toggleDieFail()
+    {
+        setDieFail(!dieFail);
+        resetDie();
     }
 
     function formatChance()
@@ -41,9 +62,44 @@ function RandomTool()
         return (totalChance * 100) + "%";
     }
 
+    function testOdds(chanceType)
+    {
+        if (chanceType == "coin" && !coinFail)
+        {
+            if (getRandomInt(2) == 0)
+            {
+                increaseCoin();
+                increaseOdds((1/2));
+            }
+            else {
+                //toggleCoinFail(); Testing purposes for now, don't turn off 
+            }
+        }
+        else if (chanceType == "die" && !dieFail)
+        {
+            if (getRandomInt(6) == 0)
+            {
+                increaseDie();
+                increaseOdds((1/6));
+            }
+            else {
+                //toggleDieFail();
+            }
+        }
+    }
+
+    function getRandomInt(max)
+    {
+        return Math.floor(Math.random() * max);
+    }
+
     return(
         <>
-            <p>{formatChance()} chance to hit</p>
+            <p>{formatChance()} chance to get this lucky</p>
+            <button className="flip-coin" onClick={() => testOdds("coin")}>Coin Hit</button>
+            <button className="flip-coin" onClick={() => resetCoin()}>Coin Reset</button>
+            <button className="roll-die" onClick={() => testOdds("die")}>Die Roll</button>
+            <button className="roll-die" onClick={() => resetDie()}>Die Reset</button>
         </>
     );
 }
